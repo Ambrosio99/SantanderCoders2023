80f42d7ad34546bb88e85df7a1317326af5c5994
Lucas Ambrosio <lu.amb99@gmail.com>
2023-09-22 17:56:18 -0300

    html home

---
+++ modulo-04/projeto-final/script.js
@@ -0,0 +1,7 @@
+import homeScript from "./script/home.js";
+import calcScript from "./script/calc.js";
+import resultScript from "./script/result.js";
+
+homeScript();
+calcScript();
+resultScript();
