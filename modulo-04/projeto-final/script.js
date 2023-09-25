5cab75ba12a17e4c24bf1c16917c709a15a166dc
Lucas Ambrosio <lu.amb99@gmail.com>
2023-09-25 02:11:20 -0300

    inicio churrascometroc

--- modulo-04/projeto-final/script.js
+++ modulo-04/projeto-final/script.js
@@ -2,6 +2,12 @@ import homeScript from "./script/home.js";
 import calcScript from "./script/calc.js";
 import resultScript from "./script/result.js";
 
-homeScript();
-calcScript();
-resultScript();
+// homeScript();
+// const divHome = document.getElementById("divHome")
+// const buttonHome = document.getElementById("buttonHome")
+// buttonHome.addEventListener("click", () => {
+//   divHome.remove()
+//   calcScript();
+// })
+
+// resultScript();
--- modulo-04/projeto-final/script/calc.js
+++ modulo-04/projeto-final/script/calc.js
@@ -1,1 +1,22 @@
-export default function initCalc() {}
+export default function initCalc() {
+  const mainContainer = document.querySelector("main");
+
+  const div = document.createElement("div");
+  div.classList.add("container-intro");
+
+  const h1 = document.createElement("h1");
+  h1.innerText = "Churrascômetro";
+
+  const p = document.createElement("p");
+  p.innerText = "Organize melhor o seu churrasco! O Churrascômetro foi desenvolvido com o intuito de facilitar a organização dos gastos do seu churrasco, tornando o cálculo muito mais preciso e eficiente para que você e seus convidados gastem na medida certa para um ótimo churrasco."
+
+  const button = document.createElement("button");
+  button.innerHTML = `Calcular seu churrasco <span class="span-button">➜</span>`;
+
+  mainContainer.appendChild(div);
+
+  div.appendChild(h1);
+  div.appendChild(p);
+  div.appendChild(button);
+  div.appendChild(img);
+}
